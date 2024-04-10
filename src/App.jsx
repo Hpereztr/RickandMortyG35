d1a64a34aacfc162244890cafc19d5d5df254736
Halinson <harlinsonperez@gmail.com>
2024-04-10 18:48:00 -0500

    entrgable3

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import useFetch from "./hooks/useFetch";
+import getRandomNumber from "./services/getRandomNumber";
+import LocationInfo from "./components/LocationInfo";
+import ResidentCard from "./components/ResidentCard";
+import FormSearch from "./components/FormSearch";
+
+
+function App() {
+  const randomLocation = getRandomNumber(126);
+  const [locationSelected, setLocationSelected] = useState(randomLocation);
+
+  const url = `https://rickandmortyapi.com/api/location/${locationSelected || getRandomNumber(126)}`;
+
+  const [location, getLocation, hasError] = useFetch(url);
+
+  useEffect(() => {
+    getLocation();
+  }, [locationSelected]);
+
+  return (
+    <div className="app">
+      <header className="app__image"><img src="../img/Frame 259.png" alt="" /></header>
+      <FormSearch 
+      setLocationSelected={setLocationSelected} 
+      />
+      {
+        hasError
+          ?<h2 className="app__error">✖️ Hey! you must provide an id from 1 to 126 🥲</h2>
+          : (
+            <>
+      <LocationInfo 
+      location={location} 
+      />
+      <div className="container-resident">
+        {location?.residents.map((url) => (
+          <ResidentCard 
+          key={url} 
+          url={url} 
+          />
+        ))}
+      </div>
+            
+      </>
+          )
+      }
+    </div>
+  );
+}
+
+export default App;
